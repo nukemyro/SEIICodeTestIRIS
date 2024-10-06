2aa5d67d683345aea7619a83a1f1f71327ce8efa
nuke <jordan.myronuk@gmail.com>
2024-10-06 11:19:02 -0400

    fix api missing CORS config preventing chrome browser from loading url data

--- api/src/index.js
+++ api/src/index.js
@@ -1,13 +1,18 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const subdivisions = require('./subdivision.json')
+const express = require('express');
+const app = express();
+const port = 3000;
+const subdivisions = require('./subdivision.json');
+
+// prevent browser complaining about cross-origin policy
+// needed to npm install cors on api project
+var cors = require('cors');
+app.use(cors());
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.get('/v1/subdivisions', (req, res) => {
     res.send(subdivisions);
-})
+});
 
 app.listen(port, () => {
     console.log('Example app listening on port 3000!')
